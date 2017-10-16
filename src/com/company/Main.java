a59449b02018d49e5d51312f8a8bbe1fbac58794
yakuza105 <yakuza105@gmail.com>
2017-10-16 22:06:29 +0200

    msg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        int k = 4;
+        int k = 12;
 
         NeuralCell neuron = new NeuralCell();
         double tabx1[] = new double[k];
@@ -22,8 +22,26 @@ public class Main {
         tabx2[2]=1;
         tabx1[3] =1;
         tabx2[3]=1;
+        tabx1[6] =0 ;
+        tabx2[6]=0;
+        tabx1[5] =1 ;
+        tabx2[5]=0;
+        tabx1[4] =0 ;
+        tabx2[4]=1;
+        tabx1[7] =1;
+        tabx2[7]=1;
+        tabx1[11] =0 ;
+        tabx2[11]=0;
+        tabx1[9] =1 ;
+        tabx2[9]=0;
+        tabx1[10] =0 ;
+        tabx2[10]=1;
+        tabx1[8] =1;
+        tabx2[8]=1;
 
-        for(int i =0 ; i<4 ; i++){
+
+
+        for(int i =0 ; i<k ; i++){
             taby[i]= tabx1[i]+tabx2[i];
             if(taby[i] >= 2){
                 taby[i] = 1;
@@ -46,18 +64,18 @@ public class Main {
 
 
 
-             System.out.println("synapsa 1:" + neuron.getInputWeight(0)+ "\n");
+             //System.out.println("synapsa 1:" + neuron.getInputWeight(0)+ "\n");
 
-             System.out.println("synapsa 2:" + neuron.getInputWeight(1)+ "\n");
+             //System.out.println("synapsa 2:" + neuron.getInputWeight(1)+ "\n");
 
-            System.out.println("powinno byc :" + taby[i] + "\n");
+           // System.out.println("powinno byc :" + taby[i] + "\n");
             int licznik = 0;
             while (neuron.getMembranePotential() - taby[i]> 0.00001 || neuron.getMembranePotential() -taby[i] <= -   0.0001) {
                 licznik++;
 
 
-                w1 = neuron.ucz(tabx1[i], neuron.getInputWeight(0), neuron.getMembranePotential(), taby[i], 0.1);
-                w2 = neuron.ucz(tabx2[i], neuron.getInputWeight(1), neuron.getMembranePotential(), taby[i], 0.1);
+                w1 = neuron.ucz(tabx1[i], neuron.getInputWeight(0), neuron.getMembranePotential(), taby[i], 0.001);
+                w2 = neuron.ucz(tabx2[i], neuron.getInputWeight(1), neuron.getMembranePotential(), taby[i], 0.001);
 
                 neuron.edit_synapses(w1, 0);
                 neuron.edit_synapses(w2, 1);
@@ -66,24 +84,24 @@ public class Main {
 
 
             }
-            if (licznik < 4 && licznik != 0)
+            if (licznik < 0 && licznik != 0)
                 i = k + 1;
             System.out.println("licznik :" + licznik + "\n");
 
 
 
-            System.out.println("ptencjał :" + neuron.getMembranePotential() + "\n");
+           // System.out.println("ptencjał :" + neuron.getMembranePotential() + "\n");
         }
-        System.out.println("synapsa 1:" + neuron.getInputWeight(0) + "\n");
+        //System.out.println("synapsa 1:" + neuron.getInputWeight(0) + "\n");
 
-        System.out.println("synapsa 2:" + neuron.getInputWeight(1) + "\n");
+        //System.out.println("synapsa 2:" + neuron.getInputWeight(1) + "\n");
 
-        double xa = 0;
+        double xa = 1;
          double xb = 1;
         neuron.setInputData(0, xa);
         neuron.setInputData(1, xb);
-        System.out.println("powinno byc :" + 0 +"\n");
-        System.out.println("ptencjał :" + neuron.finalizeData(neuron.getMembranePotential()) + "\n");
+       // System.out.println("powinno byc :" + 0 +"\n");
+        //System.out.println("ptencjał :" + neuron.finalizeData(neuron.getMembranePotential()) + "\n");
 
     }
 }
--- src/com/company/Neuralcell.java
+++ src/com/company/Neuralcell.java
@@ -6,25 +6,17 @@ import java.util.concurrent.ThreadLocalRandom;
 
 class NeuralCell
 {
-    /**
-     * NeuralCell Ciało
-     */
+
     private List<Double> Dendrites;
     private List<Double> Synapses;
 
-    /**
-     * Kontruktor klasy.
-     */
+
     public NeuralCell()
     {
         Dendrites = new ArrayList<>(100);
         Synapses = new ArrayList<>(100);
     }
 
-    /**
-
-     	Dane po obróbce finalnych kalkulacji.
-     */
 
         public double finalizeData(double membranePotential )
         {
@@ -45,24 +37,17 @@ class NeuralCell
         }
 
 
-    /**
-     *
-     * @return Ilość istniejących dendrytów i synaps.
-     */
     public int getInputSize()
     {
         return Dendrites.size();
     }
 
-    /**
-     * Dodaje nowe pole wejściowe.
-     */
+
     public void addInput(double cos)
     {
-       // Random r = new Random();
-       // double randomValue = -1 + (-1 - 0.5) * r.nextDouble();
+
         double los = ThreadLocalRandom.current().nextDouble(-1.0, 0.5);
-       // double los = r.nextDouble();
+
 
         Dendrites.add(cos);
         Synapses.add(los);
@@ -76,10 +61,8 @@ class NeuralCell
 
 
     }
-    /**
 
-     * @param //count
-     */
+
     public void addInput(double count , int cos)
     {
         for(int i = 1; i <= count; i++){
@@ -90,61 +73,37 @@ class NeuralCell
 
     }
 
-    /**
-     * Sprawdza wartość przechowywaną przez określony dendryt.
-     * @param index
-     * @return Wartość przechowywaną przez dany dendryt.
-     */
+
     public double getInputData(int index)
     {
         return Dendrites.get(index);
     }
 
-    /**
-     * Ustawia wartość przechowywaną przez dendryt o określonym indeksie.
-     * @param index
-     * @param value
-     */
+
     public void setInputData(int index, double value)
     {
         Dendrites.set(index, value);
     }
 
-    /**
-     * Sprawdza wagę przechowywaną przez określoną synapsę.
-     * @param index
-     * @return Wagę przechowywaną przez daną synapsę.
-     */
+
     public double getInputWeight(int index)
     {
         return Synapses.get(index);
     }
 
-    /**
-     * Ustawia wartość przechowywaną przez synapsę o określonym indeksie.
-     * @param index
-     * @param //value
-     */
+
     public void setInputWeight(int index, double weight)
     {
         Synapses.set(index, weight);
     }
 
-    /**
-     * Przetwarza przechowywane dane w pojedynczym węźle neuronowym,którym jest dendryt
-     * i przypisana do niego synapsa.
-     * @param index
-     * @return Wynik przetwarzania danych pojedynczego węzła.
-     */
+
     public double processCellNode(int index)
     {
         return (Dendrites.get(index)*Synapses.get(index));
     }
 
-    /**
-     * Oblicza potencjał membranowy pojedynczego neuronu.
-     * @return Potencjał membranowy lub -1 w przypadku, gdy dane wejściowe nie istnieją.
-     */
+
     public double getMembranePotential()
     {
         if(getInputSize() == 0)
@@ -157,16 +116,5 @@ class NeuralCell
         return sum;
     }
 
-    /**
-     * Oblicza wyjście pojedynczego neuronu.
-     * @return Obliczone dane wyjściowe neuronu lub -1 w przypadku, gdy dane wejściowe nie istnieją.
-     */
-   /* public double getOutput()
-    {
-        if(getInputSize() == 0)
-            return -1;
 
-        return finalizeData(getMembranePotential());
-    }
-    */
 }
